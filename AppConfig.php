1eb0a2ba8da0ec18102c77b7336cbe0e40dda3dd
DaniloRomano <daniloapromano@live.com>
2018-01-02 16:00:55 -0300

    Add files via upload

---
+++ AppConfig.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+define("DB_Host","");
+
+define("DB_Name", "");
+
+define("DB_ConnectionString","");
+
+define("DB_User","");
+
+define("DB_Pass","");
