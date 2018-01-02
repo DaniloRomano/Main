5febe15e548aff8f3cd84f975dd761c012123e99
DaniloRomano <daniloapromano@live.com>
2018-01-02 15:59:40 -0300

    Add files via upload

--- DAL/Master.php
+++ DAL/Master.php
@@ -3,8 +3,23 @@
 
 namespace DAL;
 
-public class MasterDAL{
+use TO;
 
+class MasterDAL{
+
+    private function Connection(){
+        $con=new PDO($DB_ConnectionString,$DB_User,$DB_Pass);
+        
+        return $con;
+    }
+    
+    
+    function getMasterAll(){
+        $clsMasterTO=new \TO\MasterTO();
+        $clsMasterTO->intId=1;
+        $clsMasterTO->strNome="Teste";
+        return $clsMasterTO;
+    }
 
 }
 
